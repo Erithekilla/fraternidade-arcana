a364f82c032601ceb87c4fe14650c570ba029551
Erithekilla <montassier.erick2009@gmail.com>
2025-07-03 02:08:12 -0300

    Fotos do sobre atualizado na responsividade.

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -10,18 +10,14 @@ function Sobre(){
         </div>   
         <div className={styles.sobre}>
           <div className={styles.marlyn}>
-            <div className={styles.img_marlyn}>
-              <img src={marlyn} alt="" />
-            </div>
+            <img src={marlyn} alt="" />
             <div className={styles.desc_marlyn}>
               <h2>Marlyn - Dono</h2>
               <p>Mago Cabalísta e Daemonolotra, líder da Fraternidade Arcana. Atuo com magia tem mais de 5 anos. Venho trazendo conhecimento para aqueles que buscam.</p>
             </div>
           </div>
           <div className={styles.eris}>
-            <div className={styles.img_eris}>
-              <img src={eris} alt="" />
-            </div>
+            <img src={eris} alt="" />
             <div className={styles.desc_eris}>
               <h2>Eris - SubDono</h2>
               <p>Daemonolotra praticante, criando um sistema simplificado da Daemonolatria. Atuo com a Daemonolatria desde 2024, e desde então crio técnicas para facilitar a conexão do praticante com a entidade e ter grandes resultados.</p>
