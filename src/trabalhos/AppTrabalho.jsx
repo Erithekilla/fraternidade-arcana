7403f467d8072fc32f591fb48c94bc692b6f92ce
Erithekilla <montassier.erick2009@gmail.com>
2025-07-02 23:15:25 -0300

    Atualização na rota 'Trabalhos'

--- src/trabalhos/AppTrabalho.jsx
+++ src/trabalhos/AppTrabalho.jsx
@@ -1,9 +1,13 @@
 import Trabalhos from "../components/Trabalhos"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 function App() {
     return(
         <>
+            <Header />
             <Trabalhos />
+            <Footer />
         </>
     )
 }
