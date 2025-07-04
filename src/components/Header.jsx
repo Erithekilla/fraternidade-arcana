162438d67f3bc20ff11e1c21a716c4861587d2fe
Erithekilla <montassier.erick2009@gmail.com>
2025-07-04 01:39:24 -0300

    Adicionado parte do Sobre no Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react'
 import styles from './Header.module.css';
 
 const base = import.meta.env.BASE_URL || '/fraternidade-arcana/';
 
 function Header() {
+
+    const [showSubmenu, setShowSubmenu] = useState(false);
+
+    const toggleSubmenu = () => {
+        setShowSubmenu(prev => !prev);
+    } 
+
     return (
         <header>
             <section className={styles.section}>
@@ -13,6 +21,16 @@ function Header() {
                         <li><a href="">Grupos</a></li>
                         <li><a href={`${base}oiac/`}>OIÁC</a></li>
                         <li><a href={`${base}trabalhos/`}>Trabalhos</a></li>
+                        <li><button onClick={toggleSubmenu} className={styles.btnSubmenu}>
+                            Sobre 
+                        </button>
+                        {showSubmenu &&(
+                            <ul className={styles.subMenu}>
+                                <li><a href={`${base}eris/`}>Eris</a></li>
+                                <li><a href={`${base}marlyn`}>Melvin</a></li>
+                            </ul>
+                        )}
+                        </li>
                     </ul>
                 </div>
             </section>
