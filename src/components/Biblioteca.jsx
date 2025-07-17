f85310e9c21a61cefa670330b160148288e664c2
Erithekilla <montassier.erick2009@gmail.com>
2025-07-16 22:17:53 -0300

    Finalização da primeira versão da biblioteca

--- src/components/Biblioteca.jsx
+++ src/components/Biblioteca.jsx
@@ -1,4 +1,6 @@
 import styles from './Biblioteca.module.css';
+import grimorio from '../../public/img/grimorio.jpg';
+import hermetismo_daemonolatria from '../../public/img/hermetismo_daemonolatria.jpg';
 
 function Biblioteca() {
     return(
@@ -10,26 +12,33 @@ function Biblioteca() {
             <div className={styles.biblioteca}>
                 <div className={styles.livro}>
                     <div className={styles.img}>
-
+                        <img src={grimorio} alt="" />
                     </div>
                     <div className={styles.desc}>
-                        <h2>Grimório Veritatum Aeternarum (Vol 1)</h2>
+                        <h2>Grimório Veritatum Aeternarum (Vol 1) - Marlyn</h2>
                         <p>Escrito por Marlyn e publicado gratuitamente em 2024, o Grimório Veritatum Aeternarum traz temas como Satanismo, culto a Qayn, Thelema e muito mais.</p>
                     </div>
                 </div>
                 <div className={styles.livro}>
                     <div className={styles.img}>
-
+                        <img src={hermetismo_daemonolatria} alt="" />
                     </div>
                     <div className={styles.desc}>
-                        <h2>Hermetismo e Daemonolatria</h2>
+                        <h2>Hermetismo e Daemonolatria - Eris</h2>
                         <p>Escrito por Eris, o livro traz uma reflexão sobre os conceitos Herméticos dentro da Daemonolatria, trazendo questionamentos e explicando como alguns conceitos herméticos podem ser aplicados e vistos dentro da Daemonolatria.</p>
                     </div>
                 </div>
             </div>
 
             <div className={styles.drive}>
-
+                <div className={styles.text_drive}>
+                    <p>Para encontrar todos esses livros acima, mais alguns extras, apenas clique no link abaixo!</p>
+                </div>
+                <div className={styles.link_drive}>
+                    <a href="https://drive.google.com/drive/folders/1iifhOyznkiSCyY4t4Vz0Qx-H8U4dm0h-" target="_blank">
+                        <button className={styles.btn}>Clique aqui!</button>
+                    </a>
+                </div>
             </div>
         </section>
         </>
