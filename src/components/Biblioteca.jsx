d5be1a7319dba6fe6dc875091d2f63d2943a0840
Erithekilla <montassier.erick2009@gmail.com>
2025-07-17 22:38:06 -0300

    Adicionado botão de Download Direto em dois livros da biblioteca

--- src/components/Biblioteca.jsx
+++ src/components/Biblioteca.jsx
@@ -17,6 +17,7 @@ function Biblioteca() {
                     <div className={styles.desc}>
                         <h2>Grimório Veritatum Aeternarum (Vol 1) - Marlyn</h2>
                         <p>Escrito por Marlyn e publicado gratuitamente em 2024, o Grimório Veritatum Aeternarum traz temas como Satanismo, culto a Qayn, Thelema e muito mais.</p>
+                        <a href="https://drive.google.com/file/d/1iZA4T1dkLnUR-W70ncPtNQd7GeVUWZZr/view?usp=drive_link" target="_blank"><button className={styles.btn}>Download</button></a>
                     </div>
                 </div>
                 <div className={styles.livro}>
@@ -26,6 +27,7 @@ function Biblioteca() {
                     <div className={styles.desc}>
                         <h2>Hermetismo e Daemonolatria - Eris</h2>
                         <p>Escrito por Eris, o livro traz uma reflexão sobre os conceitos Herméticos dentro da Daemonolatria, trazendo questionamentos e explicando como alguns conceitos herméticos podem ser aplicados e vistos dentro da Daemonolatria.</p>
+                        <a href="https://drive.google.com/file/d/1qrKaIQu0aBEDapZfybgxvf-Nw31fFaQl/view?usp=drive_link" target="_blank"><button className={styles.btn}>Download</button></a>
                     </div>
                 </div>
             </div>
