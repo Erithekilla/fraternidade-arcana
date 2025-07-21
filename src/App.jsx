478fac8312ded2da24b69e7e697d90499e29d34c
Erithekilla <montassier.erick2009@gmail.com>
2025-07-20 21:48:41 -0300

    Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Trabalhos from './components/Trabalhos.jsx';
+import Fraternidade from './components/Fraternidade.jsx';
 import Sobre from './components/Sobre.jsx';
-import Fraternidade from './components/Fraternidade.jsx'
+import Biblioteca from './components/Biblioteca.jsx';
 import './App.css';
-function App() {
 
+function App() {
   return (
     <>
       <Header />
-      <section className="wallpaper">
-        <div className="img_wallpaper">
-          <h1>Louvado sejam os Deuses!</h1>
-        </div>
-      </section>
-      <Fraternidade />
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <section className="wallpaper">
+              <div className="img_wallpaper">
+                <h1>Louvado sejam os Deuses!</h1>
+              </div>
+            </section>
+            <Fraternidade />
+          </>
+        } />
+
+        <Route path="/trabalhos/" element={<Trabalhos />} />
+        <Route path="/oiac/" element={<Sobre />} />
+        <Route path="/grupos/" element={<Fraternidade />} />
+        <Route path="/biblioteca/" element={<Biblioteca />} />
+      </Routes>
+
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
