70b7b33e76a7e820b0b44cf0a4ae48798e84bf53
Eris <montassier.erick2009@gmail.com>
2026-04-14 10:17:06 -0300

    Atualizacao nos links

--- src/pages/Trabalhos.jsx
+++ src/pages/Trabalhos.jsx
@@ -18,7 +18,7 @@ function Trabalhos(){
                         </div>
                         <img src={tarot} alt="Tarot" />
                         <div className={styles.link_card}>
-                            <a href="https://w.app/marlyn" target="_blank">Clique para contato!</a>
+                            <a href="https://wa.me/556192656585?text=Olá!%20Gostaria%20de%20agendar%20uma%20tiragem%20de%20tarot%20e%20conhecer%20as%20previsões%20das%20cartas." target="_blank">Clique para contato!</a>
                         </div>
                     </div>
                 </div>
@@ -29,7 +29,7 @@ function Trabalhos(){
                         </div>
                         <img src={sonhos} alt="Sonhos" />
                         <div className={styles.link_card}>
-                            <a href="https://w.app/marlyn" target="_blank">Clique para contato!</a>
+                            <a href="https://wa.me/556192656585?text=Olá!%20Gostaria%20de%20entender%20melhor%20um%20sonho%20que%20tive%20e%20solicitar%20uma%20interpretação." target="_blank">Clique para contato!</a>
                         </div>
                     </div>
                 </div>
@@ -40,7 +40,7 @@ function Trabalhos(){
                         </div>
                         <img src={numerologia} alt="Numerologia" />
                         <div className={styles.link_card}>
-                            <a href="https://w.app/marlyn" target="_blank">Clique para contato!</a>
+                            <a href="https://wa.me/556192656585?text=Olá!%20Tenho%20interesse%20em%20conhecer%20meu%20mapa%20numerológico%20e%20o%20que%20os%20números%20dizem%20sobre%20mim." target="_blank">Clique para contato!</a>
                         </div>
                     </div>
                 </div>
@@ -51,7 +51,7 @@ function Trabalhos(){
                         </div>
                         <img src={vidas_passadas} alt="Vidas passadas" />
                         <div className={styles.link_card}>
-                            <a href="https://w.app/marlyn" target="_blank">Clique para contato!</a>
+                            <a href="https://wa.me/556192656585?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20o%20atendimento%20de%20regressão%20e%20conhecer%20minhas%20vidas%20passadas." target="_blank">Clique para contato!</a>
                         </div>
                     </div>
                 </div>
